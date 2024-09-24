ba193f83bf95ca033e834a1775afeca72d88c5cc
AchroDev <achrodev@proton.me>
2024-09-23 22:10:10 -0500

    Added Printable interface and refactored

--- main.cpp
+++ main.cpp
@@ -3,11 +3,19 @@
 
 // A "Pure Virtual Function" is an Interface or a class that consists of unemplemented methods (sort of like a template)
 
-// Example Entity class
-class Entity
+// Technially an interface because of the pure virtual function, but still just a class
+class Printable
+{
+public:
+    virtual std::string GetClassName() = 0;
+};
+
+// Example Entity class now inheriting the Printable class
+class Entity : public Printable
 {
 public:
     virtual std::string GetName() { return "Entity"; } // now declared as a "pure virtual function" requiring a subclass to implement it
+    std::string GetClassName() override { return "Entity"; }
 };
 
 // Example Player class that inherits the Entity class, i.e. subclass
@@ -24,6 +32,7 @@ public:
 
     // method 'GetName' again to return the name that is the member
     std::string GetName() override { return m_Name; } // using 'override' helps to prevent bugs and know that it is overwriting something
+    std::string GetClassName() override { return "Player"; }
 };
 
 // Example function showing the issue a bit better as anything with PrintName will use the Type 'Entity'
@@ -32,17 +41,25 @@ void PrintName(Entity *entity)
     std::cout << entity->GetName() << std::endl;
 };
 
+void Print(Printable *obj)
+{
+    std::cout << obj->GetClassName() << std::endl;
+}
+
 int main()
 {
     // created new Entity
     Entity *e = new Entity;
     // print the GetName from the Entity
-    PrintName(e);
+    // PrintName(e);
 
     // created new Player
     Player *p = new Player("AchroDev");
     // print the GetName from the Player member
-    PrintName(p);
+    // PrintName(p);
+
+    Print(e);
+    Print(p);
 
     std::cin.get();
 }
